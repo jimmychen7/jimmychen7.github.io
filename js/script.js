3d377e0152e451383a8eeaaeecad2b38f6ff0f05
Jimmy <jimmy_chen_7@hotmail.com>
2018-02-04 23:53:57 +1100

    Using box-shadow animation

--- js/script.js
+++ js/script.js
@@ -1,135 +1,69 @@
+var s = document.createElement('style');
+document.head.appendChild(s);
 
-var $body = document.body,
-		$wrap = document.getElementById('wrap'),
+var cx = window.innerWidth / 2;
+var cy = window.innerHeight / 2;
+var rad = Math.PI / 180;
 
-		areawidth = window.innerWidth,
-		areaheight = window.innerHeight,
+var R = 150,
+  r = R / 16,
+  kx = 5,
+  ky = 6,
+  phi = Math.PI / 2,
+  x, y, t;
 
-		canvassize = 500,
+function pointOnLemniscate() {
+  x = (R * Math.cos(t) / (1 + (Math.sin(t) * Math.sin(t)))).toFixed(5),
+    y = (R * Math.sin(t) * Math.cos(t) / (1 + (Math.sin(t) * Math.sin(t)))).toFixed(5);
+  return o = {
+    x: x,
+    y: y
+  }
+}
 
-		length = 30,
-		radius = 5.6,
+function pointOnLissajous() {
+  x = (R * Math.sin(kx * t + phi)).toFixed(5);
+  y = (R * Math.sin(ky * t)).toFixed(5);
+  return o = {
+    x: x,
+    y: y
+  }
+}
 
-		rotatevalue = 0.035,
-		acceleration = 0,
-		animatestep = 0,
-		toend = false,
+function pointOnHeart() {
+  x = (16 * r * (Math.sin(t) * Math.sin(t) * Math.sin(t))).toFixed(5);
+  y = (-13 * r * Math.cos(t) +
+    5 * r * Math.cos(2 * t) +
+    2 * r * Math.cos(3 * t) +
+    r * Math.cos(4 * t)).toFixed(5);
+  return o = {
+    x: x,
+    y: y
+  }
+}
 
-		pi2 = Math.PI*2,
+function getShadow(pointOnCurve) {
+  var boxShadowRy = [];
+  for (var a = 0; a < 360; a += .5) {
+    t = a * rad;
+    var o = pointOnCurve();
+    boxShadowRy.push(o.x + "px" + " " + o.y + "px" + " 0px 1px hsl(" + a + ",100%,50%)");
+  }
+  var boxShadowStr = boxShadowRy.join();
+  return boxShadowStr;
+}
 
-		group = new THREE.Group(),
-		mesh, ringcover, ring,
+s.textContent = 'body::before{';
+s.textContent += 'left:' + cx + 'px;';
+s.textContent += 'top:' + cy + 'px;';
+s.textContent += 'box-shadow:' + getShadow(pointOnLissajous);
+s.textContent += '}';
 
-		camera, scene, renderer;
-
-
-	camera = new THREE.PerspectiveCamera(65, 1, 1, 10000);
-	camera.position.z = 150;
-
-	scene = new THREE.Scene();
-	// scene.add(new THREE.AxisHelper(30));
-	scene.add(group);
-
-	mesh = new THREE.Mesh(
-		new THREE.TubeGeometry(new (THREE.Curve.create(function() {},
-			function(percent) {
-
-				var x = length*Math.sin(pi2*percent),
-					y = radius*Math.cos(pi2*3*percent),
-					z, t;
-
-				t = percent%0.25/0.25;
-				t = percent%0.25-(2*(1-t)*t* -0.0185 +t*t*0.25);
-				if (Math.floor(percent/0.25) == 0 || Math.floor(percent/0.25) == 2) {
-					t *= -1;
-				}
-				z = radius*Math.sin(pi2*2* (percent-t));
-
-				return new THREE.Vector3(x, y, z);
-
-			}
-		))(), 200, 1.1, 2, true),
-		new THREE.MeshBasicMaterial({
-			color: 0xffffff
-			// , wireframe: true
-		})
-	);
-	group.add(mesh);
-
-	ringcover = new THREE.Mesh(new THREE.PlaneGeometry(50, 15, 1), new THREE.MeshBasicMaterial({color: 0xd1684e, opacity: 0, transparent: true}));
-	ringcover.position.x = length+1;
-	ringcover.rotation.y = Math.PI/2;
-	group.add(ringcover);
-
-	ring = new THREE.Mesh(new THREE.RingGeometry(4.3, 5.55, 32), new THREE.MeshBasicMaterial({color: 0xffffff, opacity: 0, transparent: true}));
-	ring.position.x = length+1.1;
-	ring.rotation.y = Math.PI/2;
-	group.add(ring);
-
-	// fake shadow
-	(function() {
-		var plain, i;
-		for (i = 0; i < 10; i++) {
-			plain = new THREE.Mesh(new THREE.PlaneGeometry(length*2+1, radius*3, 1), new THREE.MeshBasicMaterial({color: 0xd1684e, transparent: true, opacity: 0.13}));
-			plain.position.z = -2.5+i*0.5;
-			group.add(plain);
-		}
-	})();
-
-	renderer = new THREE.WebGLRenderer({
-		antialias: true
-	});
-	renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize(canvassize, canvassize);
-	renderer.setClearColor('#d1684e');
-
-	$wrap.appendChild(renderer.domElement);
-
-	$body.addEventListener('mousedown', start, false);
-	$body.addEventListener('touchstart', start, false);
-	$body.addEventListener('mouseup', back, false);
-	$body.addEventListener('touchend', back, false);
-
-	animate();
-
-
-	function start() {
-		toend = true;
-	}
-	
-	function back() {
-		toend = false;
-	}
-
-	function tilt(percent) {
-		group.rotation.y = percent*0.5;
-	}
-
-	function render() {
-
-		var progress;
-
-		animatestep = Math.max(0, Math.min(240, toend ? animatestep+1 : animatestep-4));
-		acceleration = easing(animatestep, 0, 1, 240);
-
-		if (acceleration > 0.35) {
-			progress = (acceleration-0.35)/0.65;
-			group.rotation.y = -Math.PI/2 *progress;
-			group.position.z = 50*progress;
-			progress = Math.max(0, (acceleration-0.97)/0.03);
-			mesh.material.opacity = 1-progress;
-			ringcover.material.opacity = ring.material.opacity = progress;
-			ring.scale.x = ring.scale.y = 0.9 + 0.1*progress;
-		}
-
-		renderer.render(scene, camera);
-
-	}
-
-	function animate() {
-		mesh.rotation.x += rotatevalue + acceleration;
-		render();
-		requestAnimationFrame(animate);
-	}
-
-	function easing(t,b,c,d) {if((t/=d/2)<1)return c/2*t*t+b;return c/2*((t-=2)*t*t+2)+b;}
+var vendors = [ /*'-ms-', '-moz-',*/ '-webkit-', /*'-o-',*/  ''];
+for (var i = 0; i < vendors.length; i++) {
+  s.textContent += '@' + vendors[i] + 'keyframes testAnimacion {';
+  s.textContent += '50% {' + vendors[i] + 'box-shadow:' + getShadow(pointOnLemniscate) + ';background-color:hsl(90,100%,50%);}';
+  s.textContent += '55% {background-color:transparent;}';
+  s.textContent += '100% {' + vendors[i] + 'box-shadow:' + getShadow(pointOnHeart) + ';background-color:transparent;}';
+  s.textContent += '}';
+}
